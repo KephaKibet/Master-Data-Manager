730be47c852a2d0ead6cd7e4ef5024309da4aa75
Kepha Kibet <kibet.kepha@live.com>
2023-03-14 13:24:19 +0300

    side-bar for material record actions (create, search..)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,31 @@ import { AppComponent } from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TasksComponent } from './tasks/tasks.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { GetStartedComponent } from './get-started/get-started.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SideBarComponent,
+    TasksComponent,
+    NotificationsComponent,
+    GetStartedComponent,
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     NoopAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
