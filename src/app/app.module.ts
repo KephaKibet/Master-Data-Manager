601b15cd848ed7fead818ff4e485674b082bf44a
Kepha Kibet <kibet.kepha@live.com>
2023-03-18 12:45:44 +0300

    sidebar html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +30,8 @@ import { GetStartedComponent } from './get-started/get-started.component';
     NoopAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
