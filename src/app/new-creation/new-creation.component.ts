26f71cf9628f07b6f4e7b57b9796bd9d36656f52
Kepha Kibet <kibet.kepha@live.com>
2023-04-04 23:40:49 +0300

    new material form

--- src/app/new-creation/new-creation.component.ts
+++ src/app/new-creation/new-creation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -9,15 +10,55 @@ import { NgForm} from '@angular/forms';
 })
 export class NewCreationComponent implements OnInit {
 
-  constructor() { 
-    let ERPNumber:string;
-  }
-  ngOnInit(): void {
-    
-  }
-  
-  onNewMaterialCreation(newMaterialForm: NgForm) {
-    console.log(newMaterialForm.value);
-  }
+  newMaterialForm: FormGroup = new FormGroup(
+    {
+      ERPNumbe: new FormControl('', [Validators.required]),
+     descriptor: new FormControl('', [Validators.required, Validators.email]),
+      specificatiions: new FormControl('', [
+        Validators.required,
+      ]),
+    }
+  );
 
+  constructor(
+    private httpClient: HttpClient,
+  ) {}
+
+  ngOnInit(): void {}
+
+  onNewMaterialCreation() {
+    console.log('newMaterialForm', this.newMaterialForm.value);
+
+    this.httpClient
+      .post(
+        'https://master-data-manager-mdm-default-rtdb.firebaseio.com/',
+        this.newMaterialForm.value
+      )
+      .subscribe(
+        (response) => {
+          console.log('response', response);
+          this.newMaterialForm.reset();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
+
+//   constructor(private HttpClient:HttpClient) { 
+   
+//   }
+//   ngOnInit(): void {
+    
+//   }
+  
+//   onNewMaterialCreation(newMaterialForm: NgForm) {
+//     console.log(newMaterialForm.value);
+
+//     this.HttpClient.post(
+//       'https://master-data-manager-mdm-default-rtdb.firebaseio.com/'.this.newMaterialForm.value
+    
+//   }
+
+// }
